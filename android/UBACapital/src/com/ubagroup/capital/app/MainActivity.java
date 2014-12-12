e03d3cde8d3ff4234680cffaeca0a903cf4e5660
saladthieves <muhiregide@gmail.com>
2014-12-12 23:54:02 +0300

    added main activity

---
+++ android/UBACapital/src/com/ubagroup/capital/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ubagroup.capital.app;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Main activity has been rescued!!
+ */
+public class MainActivity extends FragmentActivity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState); 
+		setContentView(R.layout.activity_main);
+	}
+}
