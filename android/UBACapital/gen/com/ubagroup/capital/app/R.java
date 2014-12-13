921a2aa67f3ebbb57b6884b15741513e950f663f
JohnAdamsy <adams.opiyo@gmail.com>
2014-12-13 07:41:36 +0300

    Design (4 screens): trade_profile,news_listing,news_listing_item,news_listing_item_details

--- android/UBACapital/gen/com/ubagroup/capital/app/R.java
+++ android/UBACapital/gen/com/ubagroup/capital/app/R.java
@@ -22,23 +22,107 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg_nse=0x7f020000;
+        public static final int comments_bubble_16=0x7f020001;
+        public static final int custom_ratings=0x7f020002;
+        public static final int favorite_16=0x7f020003;
+        public static final int ic_investnow=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int ic_marker=0x7f020006;
+        public static final int layer_card_background=0x7f020007;
+        public static final int news_arrow=0x7f020008;
+        public static final int news_item_bg=0x7f020009;
+        public static final int news_item_image=0x7f02000a;
+        public static final int oval_shape=0x7f02000b;
+        public static final int round_rectangle=0x7f02000c;
+        public static final int search=0x7f02000d;
+        public static final int star=0x7f02000e;
+        public static final int star_empty=0x7f02000f;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080027;
+        public static final int contentPostTime=0x7f080011;
+        public static final int content_frame=0x7f080014;
+        public static final int icon_map_marker=0x7f08001a;
+        public static final int inputUserQuery=0x7f080002;
+        public static final int label_account_balance=0x7f08001d;
+        public static final int label_account_balance_value=0x7f08001e;
+        public static final int label_account_ranking=0x7f080025;
+        public static final int label_account_ranking_value=0x7f080026;
+        public static final int label_account_state=0x7f080021;
+        public static final int label_account_state_value=0x7f080022;
+        public static final int label_news_item_category=0x7f080006;
+        public static final int label_news_item_title=0x7f080007;
+        public static final int layout_account_breakdown=0x7f08001b;
+        public static final int linearLayoutAccountBalance=0x7f08001c;
+        public static final int linearLayoutAccountRanking=0x7f080024;
+        public static final int linearLayoutAccountState=0x7f080020;
+        public static final int list_browse_news=0x7f080003;
+        public static final int mainItemImage=0x7f08000b;
+        public static final int news_item_icon=0x7f080005;
+        public static final int news_rating=0x7f080009;
         public static final int pager=0x7f080000;
+        public static final int postTitle=0x7f080010;
+        public static final int profile_account_location=0x7f080019;
+        public static final int profile_account_type=0x7f080018;
+        public static final int profile_info_holder=0x7f080016;
+        public static final int profile_name=0x7f080017;
+        public static final int rel_layout_news=0x7f080001;
+        public static final int relativeDesc=0x7f08000f;
+        public static final int relativeIcons=0x7f08000c;
+        public static final int relativePricing=0x7f080012;
+        public static final int scrollViewEditProfile=0x7f08000a;
+        public static final int separator_one=0x7f08001f;
+        public static final int separator_two=0x7f080023;
+        public static final int settingsLayout_picture=0x7f080004;
+        public static final int textAddCart=0x7f08000d;
+        public static final int textComments=0x7f08000e;
+        public static final int textItemAmountDiscount=0x7f080013;
+        public static final int ubacapital_logo=0x7f080015;
+        public static final int view_details_arrow=0x7f080008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int news_listing=0x7f030001;
+        public static final int news_listing_item=0x7f030002;
+        public static final int news_listing_item_details=0x7f030003;
+        public static final int trading_profile=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int account_balance_value=0x7f050007;
+        public static final int account_ranking_value=0x7f05000b;
+        public static final int account_state_value=0x7f050009;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int comments_count=0x7f050011;
+        public static final int contentPostTime=0x7f050012;
+        /**  common strings 
+         */
+        public static final int default_content_description=0x7f050002;
+        /**  news 
+         */
+        public static final int hint_search=0x7f05000c;
+        public static final int label_account_balance=0x7f050006;
+        public static final int label_account_ranking=0x7f05000a;
+        public static final int label_account_state=0x7f050008;
+        public static final int label_details_news_title=0x7f05000f;
+        public static final int label_news_item_category=0x7f05000e;
+        public static final int label_profile_account_location=0x7f050005;
+        public static final int label_profile_account_type=0x7f050004;
+        /**  trading account profile 
+         */
+        public static final int label_profile_name=0x7f050003;
+        public static final int likes_count=0x7f050010;
+        public static final int newsPostContent=0x7f050013;
+        public static final int news_item_title=0x7f05000d;
     }
     public static final class style {
+        /**  profile account text 
+         */
+        public static final int AccountProfileText=0x7f060004;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -65,5 +149,12 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        /**  news/content rating  style 
+         */
+        public static final int contentRatingBar=0x7f060005;
+        public static final int layoutSeparator=0x7f060003;
+        /**  Layout Custom Style 
+         */
+        public static final int layoutStyle=0x7f060002;
     }
 }
